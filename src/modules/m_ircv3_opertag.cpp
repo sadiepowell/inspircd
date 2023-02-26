68812fb5396dad5309ec4fd6ffcc939697039a5c
Sadie Powell <sadie@witchery.services>
2023-02-26 18:11:58 +0000

    Add the ircv3_opertag module.

---
+++ src/modules/m_ircv3_opertag.cpp
@@ -0,0 +1,124 @@
+/*
+ * InspIRCd -- Internet Relay Chat Daemon
+ *
+ *   Copyright (C) 2023 Sadie Powell <sadie@witchery.services>
+ *
+ * This file is part of InspIRCd.  InspIRCd is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "inspircd.h"
+#include "modules/cap.h"
+
+class SharedData final
+{
+public:
+	// The oper-tag capability.
+	Cap::Capability opertagcap;
+
+	// Reference to the hideoper mode.
+	UserModeReference hideopermode;
+
+	SharedData(Module* mod)
+		: opertagcap(mod, "draft/oper-tag")
+		, hideopermode(mod, "hideoper")
+	{
+	}
+};
+
+class OperTag final
+	: public ClientProtocol::MessageTagProvider
+{
+private:
+	SharedData& data;
+
+public:
+	// <opertags:hideopername> from the config.
+	bool hideopername;
+
+	OperTag(Module* mod, SharedData& sd)
+		: ClientProtocol::MessageTagProvider(mod)
+		, data(sd)
+	{
+	}
+
+	void OnPopulateTags(ClientProtocol::Message& msg) override
+	{
+		const User* user = msg.GetSourceUser();
+		if (user && user->IsOper() && !user->IsModeSet(data.hideopermode))
+			msg.AddTag("draft/oper", this, hideopername ? "" : user->oper->GetName());
+	}
+
+	bool ShouldSendTag(LocalUser* user, const ClientProtocol::MessageTagData& tagdata) override
+	{
+		return data.opertagcap.IsEnabled(user);
+	}
+};
+
+class OperRoleTag final
+	: public ClientProtocol::MessageTagProvider
+{
+private:
+	SharedData& data;
+
+public:
+	// <security:genericoper> from the config.
+	bool genericoper;
+
+	OperRoleTag(Module* mod, SharedData& sd)
+		: ClientProtocol::MessageTagProvider(mod)
+		, data(sd)
+	{
+	}
+
+	void OnPopulateTags(ClientProtocol::Message& msg) override
+	{
+		const User* user = msg.GetSourceUser();
+		if (user && user->IsOper() && !user->IsModeSet(data.hideopermode))
+			msg.AddTag("draft/oper-role", this, user->oper->GetType());
+	}
+
+	bool ShouldSendTag(LocalUser* user, const ClientProtocol::MessageTagData& tagdata) override
+	{
+		return !genericoper && data.opertagcap.IsEnabled(user);
+	}
+};
+
+class ModuleIRCv3OperTag final
+	: public Module
+{
+private:
+	SharedData data;
+	OperTag opertag;
+	OperRoleTag operroletag;
+
+public:
+	ModuleIRCv3OperTag()
+		: Module(VF_VENDOR, "Provides the draft IRCv3 oper and oper-role message tags.")
+		, data(this)
+		, opertag(this, data)
+		, operroletag(this, data)
+	{
+	}
+
+	void ReadConfig(ConfigStatus& status) override
+	{
+		const auto& security = ServerInstance->Config->ConfValue("security");
+		operroletag.genericoper = security->getBool("genericoper");
+
+		const auto& opertags = ServerInstance->Config->ConfValue("opertags");
+		opertag.hideopername = opertags->getBool("hideopername", operroletag.genericoper);
+	}
+};
+
+MODULE_INIT(ModuleIRCv3OperTag)
